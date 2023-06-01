a7ba497fe1bbe6d167abc88efee78d7ec890e494
Xiangyu Pang <xiangyupang@huanle.com>
2023-06-01 11:25:06 +0800

    修改registerInfos列表为null报错

--- unity/Assets/core/upm/Editor/Src/Generator/FileExporter.cs
+++ unity/Assets/core/upm/Editor/Src/Generator/FileExporter.cs
@@ -205,6 +205,14 @@ namespace Puerts.Editor
                 }
                 
                 var RegisterInfos = RegisterInfoGenerator.GetRegisterInfos(genTypes);
+				List<RegisterInfoForGenerate> notNullRegisterInfos = new List<RegisterInfoForGenerate>();
+                foreach(var infos in notNullRegisterInfos)
+                {
+                    if(null != infos)
+                    {
+                        notNullRegisterInfos.Add(infos);
+                    }
+                }
 
                 if (loader == null)
                 {
@@ -213,7 +221,7 @@ namespace Puerts.Editor
                 using (var jsEnv = new JsEnv(loader))
                 {
                     var registerInfoRender = jsEnv.ExecuteModule<Func<List<RegisterInfoForGenerate>, string>>("puerts/templates/registerinfo.tpl.mjs", "default");
-                    string registerInfoContent = registerInfoRender(RegisterInfos);
+                    string registerInfoContent = registerInfoRender(notNullRegisterInfos);
                     var registerInfoPath = outDir + "RegisterInfo_Gen.cs";
                     using (StreamWriter textWriter = new StreamWriter(registerInfoPath, false, Encoding.UTF8))
                     {
